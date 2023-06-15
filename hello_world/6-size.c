7e101c35ebf5fac93c3810f8f0bba9e8630f595a
MateoZouko <mateozouko1@gmail.com>
2023-06-15 05:57:54 -0700

    s

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-char chatType;
+char charType;
 int intType;
 long int longintType;
 long long int longlongintType;
@@ -9,7 +9,7 @@ float floatType;
 printf("Size of char: %zu byte\n", sizeof(charType));
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of longint: %zu bytes\n", sizeof(longintType));
-printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+printf("Sizf longlongint: %zu bytes\n", sizeof(longlongintType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
