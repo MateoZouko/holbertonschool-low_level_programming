1528c43e3abaf8f40ad58ab86fff7e5a8de75d18
MateoZouko <mateozouko1@gmail.com>
2023-06-15 06:39:14 -0700

    s

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<time.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if n > 0
+	{
+		return (is positive);
+	}if else n = 0
+	{
+		return (is zero);
+	} else
+	{
+		return (is negative);
+	}
+	return (0);
+}
+
