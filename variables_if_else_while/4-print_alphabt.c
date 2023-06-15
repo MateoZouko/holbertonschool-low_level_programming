c88e54b3239fa10e41a74e156a50910771a56b73
MateoZouko <mateozouko1@gmail.com>
2023-06-15 10:24:14 -0700

    s

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		if (character == q, e)
+		{
+		putchar(character);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
