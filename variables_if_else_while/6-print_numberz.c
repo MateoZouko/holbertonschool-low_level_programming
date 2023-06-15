5e16ff91c651e93065aa1c9fa90129e0104bd525
MateoZouko <mateozouko1@gmail.com>
2023-06-15 16:06:01 -0700

    s

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,15 @@
  */
 int main()
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-        putchar(i + '0');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return 0;
 }
 
