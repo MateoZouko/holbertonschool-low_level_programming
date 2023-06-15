3531b387ce5a872941ecfb5e87c419b49edc0f60
MateoZouko <mateozouko1@gmail.com>
2023-06-15 16:08:57 -0700

    s

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
- * main - this is the main
- *
- * Return: 0 on successful execution
- */
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
 int main()
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-	}
-	
-	putchar('\n');
-
-	return 0;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
 }
 
+putchar('\n');
+
+return 0;
+}
