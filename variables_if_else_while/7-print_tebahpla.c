777d9393066d25e50c4c0a4997ec219c3534f7c5
MateoZouko <mateozouko1@gmail.com>
2023-06-15 16:14:23 -0700

    s

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+	char character;
+
+	for (character = 'z'; character >= 'a'; character--)
+	{
+		putchar(character);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
