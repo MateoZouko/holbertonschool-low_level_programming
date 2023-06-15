f1a5f6c6154e61a7661f71794a1deeed562579c7
MateoZouko <mateozouko1@gmail.com>
2023-06-15 05:50:52 -0700

    s

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<libc6-dev-i386>
+int main(void)
+{
+char chatType;
+int intType;
+longint longintType;
+longlongint longlongintType;
+float floatType;
+printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of longint: %zu bytes\n", sizeof(longintType));
+printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+return (0);
+}
