fa142dbcd6cbd751b2f72eae493c4cd62cc08318
MateoZouko <mateozouko1@gmail.com>
2023-06-15 10:54:14 -0700

    s

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-	char number;
+	int number;
 
-	for (number = '0'; number+10);
+	for (number = 0; number < 10; number++);
 	{
-		putchar(number);
+		putchar('0' + number);
 	}
 
 	putchar('\n');
