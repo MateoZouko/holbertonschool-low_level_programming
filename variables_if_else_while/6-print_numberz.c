175d21938d8b9c83b83108d9fe024b68c5283092
MateoZouko <mateozouko1@gmail.com>
2023-06-15 10:45:35 -0700

    s

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number++)
+	{
+		putchar(number);
+	}
+
+	putchar('\n')
+
+	return (0);
+}
