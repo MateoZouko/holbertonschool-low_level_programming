2ac16bee99df5e8e132a3aa80c45469375698b61
MateoZouko <mateozouko1@gmail.com>
2023-06-15 08:39:02 -0700

    s

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+	char abecedario;
+	abecedario = "abcdefghijklmnñopqrstuvwxyz";
+
+	for (int i = 0; i < 27, i++)
+	putchar("%c \n", abecedario[i]);
+	return (0);
+}
