6fa1c4e024bc7788f4f701d03b1ed819e9fdad89
MateoZouko <mateozouko1@gmail.com>
2023-06-16 11:10:25 -0700

    s

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check the code.
+ * Return: 1 if lowercase, 0 if not lowercase
+ *@c: function.
+ */
+
+int _islower(int c);
+{
+	if (c <= 97 || c > 122)
+	{
+		return (1);
+} else
+{
+	return (0);
+}
+return (0);
+}
