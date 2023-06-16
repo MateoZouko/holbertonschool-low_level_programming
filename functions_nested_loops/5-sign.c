309bbcc304fb0206bf03f21a7bb05c578b30f79b
MateoZouko <mateozouko1@gmail.com>
2023-06-16 16:31:40 -0700

    s

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,7 +7,16 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		print_sign(+1);
-
-			return (0);
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
