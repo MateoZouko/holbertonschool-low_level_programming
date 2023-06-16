09cfe03d4999ec8563432165b3eaa9e4490a2db6
MateoZouko <mateozouko1@gmail.com>
2023-06-16 05:24:00 -0700

    s

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - this is the main
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+char _putchar;
+
+{
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+}
