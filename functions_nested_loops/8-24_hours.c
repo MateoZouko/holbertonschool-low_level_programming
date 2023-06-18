b483e82ee6c9369772e9ffccc06f39136836dc71
MateoZouko <mateozouko1@gmail.com>
2023-06-18 07:35:12 -0700

    s

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print minutes.
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hora, minutos;
+
+	for (hora = 0; hora <= 23; hora++)
+	{
+
+		for (minutos = 0; minutos <= 59; minutos++)
+		{
+			_putchar('0' + hora);
+			_putchar(':');
+			_putchar('0' + minutos);
+			_putchar('\n');
+		}
+	}
+}
