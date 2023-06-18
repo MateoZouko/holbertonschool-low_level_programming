325ade0fa8844ac04124cc93da9b5f967d898fb8
MateoZouko <mateozouko1@gmail.com>
2023-06-18 07:13:21 -0700

    s

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		nv = -1 * (n % 10);
-		_putchar(nv);
+		_putchar(nv + '0');
 		return (nv);
 	}
 	else
 	{
 		nv = n % 10;
-		_putchar(nv);
+		_putchar(nv + '0');
 		return (nv);
 	}
 }
