9fea0be1e8c7069d5b1ce91aff70edc60afbdc53
MateoZouko <mateozouko1@gmail.com>
2023-06-18 08:42:39 -0700

    s

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%i", n); //Mientras n  >= a 98, resta de 1 en 1 e imprimi
+			printf("%i", n);
 			if (n != 98)
 			{
 				printf(", ");
@@ -22,10 +22,10 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%i", n); //mientras n <= a 98, reduci de 1 en 1 e imprimi
+			printf("%i", n);
 			if (n != 98)
 			{
-				printf(", "); //Si no es 98: imprime , y espacio
+				printf(", ");
 			}
 			n++;
 		}
