728508a3b2ff93d00132f7400cb7451938bde6d4
MateoZouko <mateozouko1@gmail.com>
2023-06-19 14:16:56 -0700

    s

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mu - check code.
+ * @a: integrer.
+ * @b: integrer.
+ * Return: Always.
+ */
+int mu(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	printf("%i\n", c);
+	return (0);
+}
