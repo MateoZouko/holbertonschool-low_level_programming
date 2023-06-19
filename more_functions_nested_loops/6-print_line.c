df5f7a4ef49e46dbdb34c522063d208241ded5dc
MateoZouko <mateozouko1@gmail.com>
2023-06-19 16:05:29 -0700

    s

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - straight line
+ * @n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
