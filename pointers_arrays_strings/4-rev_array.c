8ea0fc9bef5bc7826f764bf69e9f60234a7731db
MateoZouko <mateozouko1@gmail.com>
2023-06-24 13:54:54 -0700

    s

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i != '\0')
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
