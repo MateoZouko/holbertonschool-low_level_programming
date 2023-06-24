7b38ff940efdb504ce8f9c8948889b68c2d1b31c
MateoZouko <mateozouko1@gmail.com>
2023-06-24 15:44:30 -0700

    s

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,21 @@ char
 	char remp[5][2] = {{'a', '4'}, {'e', '3'},
 		{'o', '0'}, {'t', '7'}, {'l', '1'}};
 
-	while (*a != '\0')
+	while (*str != '\0')
 	{
 		int b = 0;
 
 		while (b < 5)
 		{
-		if (*a == remp[b][0] || *a == remp[b][0] - ('a' - 'A'))
+		if (*str == remp[b][0] || *a == remp[b][0] - ('a' - 'A'))
 		{
 		*a = remp[b][1];
 	break;
 		}
-	b++;
+		b++;
 		}
-	a++;
+		a++;
+		str++;
 	}
-return (a);
+return (str);
 }
