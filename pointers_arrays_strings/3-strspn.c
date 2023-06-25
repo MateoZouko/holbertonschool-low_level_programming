8972cfda3dc61c642b3ede445ac56780af2fa2c0
MateoZouko <mateozouko1@gmail.com>
2023-06-25 14:40:52 -0700

    s

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 
-int
-_strcmp(char *s1, char *s2)
+unsigned int
+_strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int length = 0;
+	int found, i;
 
-	while (s1[i] != '\0' && s1[i] == s2[i])
+	while (*s != '\0')
 	{
-		i++;
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+		{
+			break;
+		}
+		length++;
+		s++;
 	}
-	return (s1[i] - s2[i]);
+	return (length);
 }
