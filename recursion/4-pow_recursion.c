2f94bae7f04b31fbf2fdee5667c81aaf350acd42
MateoZouko <mateozouko1@gmail.com>
2023-06-28 10:09:36 -0700

    s

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int
+_pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
