be33973190b0cb324f9d1b61814f0e2bfa901e55
MateoZouko <mateozouko1@gmail.com>
2023-07-05 16:26:07 -0700

    s

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,8 +6,19 @@ void
 free_grid(int **grid, int height)
 {
 	int i;
-	for (i =0; i < height; i++)
+
+	/**
+	* Utilizo un bucle para recorrer la matriz hasta
+	* height -1 y libero la memoria asignada para
+	* cada fila de la string
+	*/
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 
+	/**
+	* utilizo la funcion free para liberar la memoria
+	* asignada al puntero grid para liberar la memoria
+	* de la matriz bidimensional totalmente
+	*/
 	free(grid);
 }
