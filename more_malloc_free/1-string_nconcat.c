a0b4e98c1cb2765918d732ef364a4def4a3d8f30
MateoZouko <mateozouko1@gmail.com>
2023-07-08 16:19:14 -0700

    s

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,16 +6,16 @@
 char
 *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	unsigned int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
 	unsigned int str_length = len1 + n;
 	char *str;
-
-	if (s1 == NULL)
-		s1 = " ";
-
-	if (s2 == NULL)
-		s2 = " ";
 
 	if (n >= len2)
 		n = len2;
