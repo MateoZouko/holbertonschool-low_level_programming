64287db67a7277b627ed29e529f5a827defbbafd
MateoZouko <mateozouko1@gmail.com>
2023-07-13 05:12:15 -0700

    s

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,13 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+/**
+ * sum_them_all - Returns the sum of the parameters.
+ * @n: number of parameters
+ * @...: variable number of parameters to calculate.
+ *
+ * Return: If n == 0 - 0.
+ * otherwise - the sum of parameters.
+ */
 
 int
 sum_them_all(const unsigned int n, ...)
